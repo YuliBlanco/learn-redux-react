9835f2958e7fadfdad6c348585ef969076b93e22
Yuli Blanco <blancoyule@gmail.com>
2017-07-05 23:10:59 -0400

    first part learn redux with react

---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Single = React.createClass({
+  render() {
+    return (
+	     <div className="single-photo">
+	     Im the single
+	     </div>
+    )
+  }
+});
+
+export default Single;
